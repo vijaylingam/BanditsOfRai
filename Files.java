3ee13097513bf3a93aef8be9d24bbc50c3d3c6a6
vijaylingam <vijay.lingam@ashoka.edu.in>
2016-03-18 04:46:01 +0530

    Commit 2
    printFandD method now prints the number of files directly under each
    directory.

--- Files.java
+++ Files.java
@@ -7,16 +7,33 @@ import java.lang.String;
 
 public class Files {
 
+    int files = 0;
     public void printFandD(File cwd){ //method to print files and directories
+    	int filecount = 0;
         for(File file: cwd.listFiles()){ //cwd here is the variable denoting current working directory
             if (file.isFile()) {
-                System.out.println("f   " + file); 
+
+                System.out.println("0   " + "f   " + file); 
             } 
             else {
-                System.out.println("d   " + file);
+                filecount = fileCounter(file); //recursive call to filecounter method
+                System.out.println(filecount + "	d   " + file);
                 printFandD(file);
             }
         }
+    }
+
+    public int fileCounter(File cwd){
+        files = 0;
+        for(File file: cwd.listFiles()){
+            if(file.isFile()){ //checks whether the selection is a file and returns appropriate boolean value
+                files+=1;
+            }
+            else{
+                files += 0;
+            }
+        }
+        return files;
     }
 
     
